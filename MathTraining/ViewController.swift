18955bcf7595d408b9611a8a09717cbeb99216cb
Valentin Sanchez <elzero_2@hotmail.com>
2020-03-11 00:42:29 +0100

    Initial Commit

---
+++ MathTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathTraining
+//
+//  Created by Valentin Sanchez on 11/03/2020.
+//  Copyright © 2020 Valentin Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
